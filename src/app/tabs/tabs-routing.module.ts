e13ab8205af55ca73ae67639c62d861c82b10ce5
adicnk <adicoolnkeren@gmail.com>
2022-05-06 21:22:16 +0700

    Menambahkan Login

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,28 +8,28 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'chart',
+        loadChildren: () => import('../chart/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'buy',
+        loadChildren: () => import('../buy/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'info',
+        loadChildren: () => import('../info/tab3.module').then(m => m.Tab3PageModule)
       },
       {
-        path: 'tab4',
-        loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
+        path: 'news',
+        loadChildren: () => import('../news/tab4.module').then(m => m.Tab4PageModule)
       },
       {
-        path: 'tab5',
-        loadChildren: () => import('../tab5/tab5.module').then(m => m.Tab5PageModule)
+        path: 'menu',
+        loadChildren: () => import('../menu/tab5.module').then(m => m.Tab5PageModule)
       },
       {
         path: '',
-        redirectTo: 'tab1',
+        redirectTo: 'chart',
         pathMatch: 'full'
       }
     ]

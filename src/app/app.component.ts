7c8fbbadcfc645c2f1b1536f9fc43c01c709525b
adicnk <adicoolnkeren@gmail.com>
2022-05-06 18:40:26 +0700

    Menambahkan Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(
+    private storage: Storage,
+    public navCtrl : NavController
+    ) {
+  }
+
+  async ngOnInit() {
+    // If using a custom driver:
+    // await this.storage.defineDriver(MyCustomDriver)
+    await this.storage.create();
+  }
+
+  initializeApp(){
+    this.storage.get('storage_xxx').then((res)=>{
+      if (res==null) {
+        this.navCtrl.navigateRoot('/login')
+      } else {
+        this.navCtrl.navigateRoot('/home')
+      }
+    });
+  }
 }

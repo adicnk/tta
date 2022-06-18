5b23b9b57b54465a64b52e03132eac39cdedad55
adicnk <adicoolnkeren@gmail.com>
2022-06-19 04:22:10 +0700

    Add Client Service

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Clients {
+  id: string;
+  user_id: string;
+  chat_id: string;
+  first_name: string;
+  last_name: string;
+  username: string;
+  status: string;
+  counter: string;
+  counter_chart: string;
+  active_until: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  private url = 'http://localhost/tta/api/clients';
+
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  gettAll(){
+
+  }
+
+  get(id: string){
+
+  }
+
+  create(clients : Clients){
+
+  }
+
+  update(clients : Clients, id: string){
+    
+  }
 }

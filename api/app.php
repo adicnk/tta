35f64e24be9c0d59bbdbd7a8baf2b28521dea392
adicnk <adicoolnkeren@gmail.com>
2022-06-18 21:21:44 +0700

    Add Backend

--- api/app.php
+++ api/app.php
@@ -4,4 +4,32 @@ require_once 'headers.php';
 
 $conn = new mysqli('localhost', 'root','','taurustr_apps');
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET[''])) {
+        $id = $conn->real_escape_string($_GET['user_id']);
+        $sql = $conn->query("SELECT * FROM bot_clients WHERE user_id = '$id'");
+        $data = $sql->fetch_assoc();
+    } else {
+        $data = array();
+        $sql = $conn->query("SELECT * FROM bot_clients");
+        while ($d = $sql->fetch_assoc()) {
+            $data[] =$d;
+        }
+    }
+    exit(json_encode($data));
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "POST";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    echo "PUT";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    echo "DELETE";
+}
+
+
 ?>

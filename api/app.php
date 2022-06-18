32965d38b9ad9c067097b6ce750e83582e73cb9b
adicnk <adicoolnkeren@gmail.com>
2022-06-18 22:22:12 +0700

    Add Backend

--- api/app.php
+++ api/app.php
@@ -46,7 +46,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    echo "DELETE";
+    if (isset($_GET['id'])) {
+        $id = $conn->real_escape_string($_GET['id']);
+        echo $id;
+        $sql = $conn->query("DELETE FROM bot_register WHERE id = '$id'");
+        if ($sql) {
+            exit(json_encode(array('status'=>'success')));
+        } else {
+            exit(json_encode(array('status'=>'error')));
+        }
+    }
 }
 
 
